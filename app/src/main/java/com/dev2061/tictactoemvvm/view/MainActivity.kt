23c944967b6143ffe7e7ed87bae24085ad732af1
Anonymous <kata2021dev2061@gmail.com>
2021-10-05 14:21:01 +0200

    Setting up data and view binding in layout and referencing it in activity

--- app/src/main/java/com/dev2061/tictactoemvvm/view/MainActivity.kt
+++ app/src/main/java/com/dev2061/tictactoemvvm/view/MainActivity.kt
@@ -2,11 +2,21 @@ package com.dev2061.tictactoemvvm.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
 import com.dev2061.tictactoemvvm.R
+import com.dev2061.tictactoemvvm.databinding.ActivityMainBinding
+import com.dev2061.tictactoemvvm.viewmodel.BoardViewModel
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var vm: BoardViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding: ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        vm = ViewModelProvider(this).get(BoardViewModel::class.java)
+        binding.vm = vm
     }
 }
