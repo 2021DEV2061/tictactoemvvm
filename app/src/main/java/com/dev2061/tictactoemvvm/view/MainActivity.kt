5d654b580ed285f98f83497a1ee69c1835d05752
Anonymous <kata2021dev2061@gmail.com>
2021-10-05 14:45:02 +0200

    Added databinding logic to layout and observed winner in activity

--- app/src/main/java/com/dev2061/tictactoemvvm/view/MainActivity.kt
+++ app/src/main/java/com/dev2061/tictactoemvvm/view/MainActivity.kt
@@ -3,9 +3,11 @@ package com.dev2061.tictactoemvvm.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.dev2061.tictactoemvvm.R
 import com.dev2061.tictactoemvvm.databinding.ActivityMainBinding
+import com.dev2061.tictactoemvvm.model.Player
 import com.dev2061.tictactoemvvm.viewmodel.BoardViewModel
 
 class MainActivity : AppCompatActivity() {
@@ -17,6 +19,9 @@ class MainActivity : AppCompatActivity() {
         val binding: ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
         vm = ViewModelProvider(this).get(BoardViewModel::class.java)
+        vm.startGame()
         binding.vm = vm
+        vm.getWinner().observe(this, Observer<Player> {
+        })
     }
 }
