592775b2f2747bfdfb478e900e1fed8c86a4e42b
Anonymous <kata2021dev2061@gmail.com>
2021-10-05 12:26:36 +0200

    TDD: added implementation for checking column, row or diagonal 3 squares matches

--- app/src/main/java/com/dev2061/tictactoemvvm/model/Board.kt
+++ app/src/main/java/com/dev2061/tictactoemvvm/model/Board.kt
@@ -10,16 +10,28 @@ class Board {
     }
 
     fun checkIfSquaresIdenticalInColumn(): Boolean {
+        for (i in 0 until BOARD_SIZE)
+            if (checkIfSquaresIdentical(squares[0][i], squares[1][i], squares[2][i])) {
+                return true
+            }
         return false
     }
 
     fun checkIfSquaresIdenticalInRow(): Boolean {
+        for (i in 0 until BOARD_SIZE)
+            if (checkIfSquaresIdentical(squares[i][0], squares[i][1], squares[i][2])) {
+                return true
+            }
         return false
     }
 
     fun checkIfSquaresIdenticalDiagonal(): Boolean {
-        return false
+        return checkIfSquaresIdentical(squares[0][0], squares[1][1], squares[2][2]) ||
+                checkIfSquaresIdentical(squares[0][2], squares[1][1], squares[2][0])
     }
+
+    private fun checkIfSquaresIdentical(vararg squares: Square?) =
+        (1 until squares.size).none { squares[0] == null || squares[0] != squares[it] }
 
     companion object {
         private const val BOARD_SIZE = 3
