db9584776a1b8f8f9f2d5b375186775f9d3cf441
Anonymous <kata2021dev2061@gmail.com>
2021-10-05 15:25:25 +0200

    Added ui tests for checking all results (X win, O win, draw)

--- app/src/androidTest/java/com/dev2061/tictactoemvvm/MainActivityTest.kt
+++ app/src/androidTest/java/com/dev2061/tictactoemvvm/MainActivityTest.kt
@@ -1,6 +1,7 @@
 package com.dev2061.tictactoemvvm
 
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.assertion.ViewAssertions
 import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.espresso.matcher.ViewMatchers.withId
@@ -18,6 +19,7 @@ class MainActivityTest {
 
     @Test
     fun checkBoardAndSquaresShown() {
+
         onView(withId(R.id.grid_id)).check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
         onView(withId(R.id.s00)).check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
         onView(withId(R.id.s01)).check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
@@ -28,5 +30,104 @@ class MainActivityTest {
         onView(withId(R.id.s20)).check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
         onView(withId(R.id.s21)).check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
         onView(withId(R.id.s22)).check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+    }
+
+    @Test
+    fun checkWinnerDiagonal() {
+
+        onView(withId(R.id.s02)).perform(ViewActions.click())
+            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+
+        onView(withId(R.id.s12)).perform(ViewActions.click())
+            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+
+        onView(withId(R.id.s11)).perform(ViewActions.click())
+            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+
+        onView(withId(R.id.s22)).perform(ViewActions.click())
+            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+
+        onView(withId(R.id.s20)).perform(ViewActions.click())
+
+        onView(ViewMatchers.withText("X won"))
+            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+    }
+
+    @Test
+    fun checkWinnerVertical() {
+
+        onView(withId(R.id.s00)).perform(ViewActions.click())
+            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+
+        onView(withId(R.id.s01)).perform(ViewActions.click())
+            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+
+        onView(withId(R.id.s10)).perform(ViewActions.click())
+            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+
+        onView(withId(R.id.s11)).perform(ViewActions.click())
+            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+
+        onView(withId(R.id.s20)).perform(ViewActions.click())
+
+        onView(ViewMatchers.withText("X won"))
+            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+    }
+
+    @Test
+    fun checkWinnerHorizontal() {
+
+        onView(withId(R.id.s00)).perform(ViewActions.click())
+            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+
+        onView(withId(R.id.s10)).perform(ViewActions.click())
+            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+
+        onView(withId(R.id.s20)).perform(ViewActions.click())
+            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+
+        onView(withId(R.id.s11)).perform(ViewActions.click())
+            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+
+        onView(withId(R.id.s22)).perform(ViewActions.click())
+            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+
+        onView(withId(R.id.s12)).perform(ViewActions.click())
+
+        onView(ViewMatchers.withText("O won"))
+            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+    }
+
+    @Test
+    fun checkDraw() {
+
+        onView(withId(R.id.s00)).perform(ViewActions.click())
+            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+
+        onView(withId(R.id.s01)).perform(ViewActions.click())
+            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+
+        onView(withId(R.id.s02)).perform(ViewActions.click())
+            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+
+        onView(withId(R.id.s11)).perform(ViewActions.click())
+            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+
+        onView(withId(R.id.s10)).perform(ViewActions.click())
+            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+
+        onView(withId(R.id.s20)).perform(ViewActions.click())
+            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+
+        onView(withId(R.id.s12)).perform(ViewActions.click())
+            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+
+        onView(withId(R.id.s22)).perform(ViewActions.click())
+            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+
+        onView(withId(R.id.s21)).perform(ViewActions.click())
+
+        onView(ViewMatchers.withText("It's a draw"))
+            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
     }
 }
